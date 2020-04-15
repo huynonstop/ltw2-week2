c5544485975a1d92e438ccb2cfc0234d3763fb7b
huynonstop <tthuykh99@gmail.com>
2020-04-15 11:08:17 +0700

    express

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const http = require('http')
+const express = require('express')
 const port = process.env.PORT || 3000
 
 const requestHandler = (request, response) => {
-    response.writeHead(200)
-    console.log(request.url)
-    response.end('Hello Node.js Server! of Tran Tuan Huy (1760327)')
+    const name = request.query.name || "Tran Tuan Huy (1760327)"
+    console.log(request.query)
+    response.send('Hello Node.js Server! of ' + name)
 }
 
-const server = http.createServer(requestHandler)
-
+const server = express()
+server.get('/', requestHandler)
 server.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
-
     console.log(`server is listening on ${port}`)
 })
