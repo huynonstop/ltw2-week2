ee6fe20bafc82de111d5d0615826da43a65c660e
huynonstop <tthuykh99@gmail.com>
2020-04-15 10:48:57 +0700

    init

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 const requestHandler = (request, response) => {
     response.writeHead(200)
     console.log(request.url)
-    response.end('Hello Node.js Server!')
+    response.end('Hello Node.js Server! of Tran Tuan Huy (1760327)')
 }
 
 const server = http.createServer(requestHandler)
